452ffc61fd517aca051b74711fbc8ecd6a4db85f
An Bui <hvbuianbui@gmail.com>
2019-12-16 00:20:25 -0800

    removed header & tested total month

--- PyBank/main_pybank.py
+++ PyBank/main_pybank.py
@@ -8,25 +8,28 @@ csvpath = os.path.join('Resources', 'budget_data.csv')
 
 with open(csvpath, newline='',encoding='UTF-8') as csvfile:
     budget_csv = csv.reader(csvfile, delimiter=',')
-    #data_header = next(budget_csv)
+    data_header = next(budget_csv)
+
+
 
     date_list = []
+    profit_losses = []
+    
     for row in budget_csv:
         
         date_list.append(row[0])
+        
+        profit_losses.append(row[1])
 
+    
+    #sum_profit = sum(int(profit_losses))
+
+    print(len(date_list))
+    print(profit_losses)
     #print(len(date_list))    
-    sum_month = len(date_list) -1
-    print(sum_month)
+    #sum_month = len(date_list) -1
+    #print(sum(int(total_profit)))
+   
+    #print(sum_month)
     
         
-        #print(date_list) - showed list of date_list
-    
-    #print(f"Total number of month: {number_of_month}")
-
-
-
-#for row in budget_csv:
-        
-    #print(row[1]) -- tested to see all rows at index 1
-    #print(row[0]) -- tested to see all rows at index 0
